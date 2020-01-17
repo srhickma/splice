2cdf3e4e6f0891d57a14ea4183445a1a28d808ff
Shane Hickman <srhickma@edu.uwaterloo.ca>
2020-01-17 03:04:11 -0500

    Persistently store associations using chrome local storage

--- splice.js
+++ splice.js
@@ -1,13 +1,22 @@
 const splice_matcher = /^[a-z]*:\/\/s\+\/(.*)$/;
 const traversal_matcher = /^[a-z]*:\/\/s\/(.*)$/;
+const local_storage_key = 'splice::mapping';
 
-var slices = {};
+var mapping = {};
+
+window.onload = function() {
+    chrome.storage.local.get(local_storage_key, (value) => {
+        mapping = value;
+    });
+}
 
 function splice(requestDetails) {
     browser.tabs.query({active: true, currentWindow: true})
         .then(tabs => {
             const key = splice_matcher.exec(requestDetails.url)[1];
-            slices[key] = tabs[0].url;
+            mapping[key] = tabs[0].url;
+
+            chrome.storage.local.set({local_storage_key: mapping});
         });
 
     return {
@@ -19,7 +28,7 @@ function traverse(requestDetails) {
     const key = traversal_matcher.exec(requestDetails.url)[1];
 
     return {
-        redirectUrl: slices[key]
+        redirectUrl: mapping[key]
     };
 }
 
